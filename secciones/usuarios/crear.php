0ac9ad505bc691706c61bc46e53c72c6ebd10679
jairowong <jairozz200@gmail.com>
2024-05-16 23:43:04 -0500

    agregando usuarios

--- secciones/usuarios/crear.php
+++ secciones/usuarios/crear.php
@@ -1,5 +1,26 @@
 
 <?php include("../../templates/header.php");?>
-Crear Empleados
+<div class="card">
+    <div class="card-header">Usuarios</div>
+    <div class="card-body">
+     <form action="" method="post" enctype="multipart/form-data">
+        <div class="mb-3">
+            <label for="nombredelpuesto" class="form-label">Nombre del usuario</label>
+            <input type="text" class="form-control" name="nombredelpuesto" id="nombredelpuesto" aria-describedby="helpId" placeholder="nombredelusuario"/>
+        </div>
+        <div class="mb-3">
+            <label for="nombredelpuesto" class="form-label">Contraseña</label>
+            <input type="password" class="form-control" name="contraseña" id="contraseña" aria-describedby="helpId" placeholder="contraseña"/>
+        </div>
+        <div class="mb-3">
+            <label for="nombredelpuesto" class="form-label">Correo</label>
+            <input type="email" class="form-control" name="email" id="email" aria-describedby="helpId" placeholder="email"/>
+        </div>
+        <button type="submit" class="btn btn-success">Agregar</button>
+        <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>     
+     </form>
+    </div>
+
+</div>
 
 <?php include("../../templates/footer.php");?>
--- secciones/usuarios/index.php
+++ secciones/usuarios/index.php
@@ -1,5 +1,39 @@
 
 <?php include("../../templates/header.php");?>
-Listado de Usuarios
+<div class="card">
+    <div class="card-header">
+
+        <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Nombre del Usuario</a>
+
+    </div>
+    <div class="card-body">
+    <div class="table-responsive-sm">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre del Usuario</th>
+                <th scope="col">Contraseña</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="">
+                <td scope="row">1</td>
+                <td>Programador Jr</td>
+                <td>******</td>
+                <td>jairozz200@gmail.com</td>
+                <td>
+                    <input name="" id="btneditar" class="btn btn-info" type="button" value="Editar"/>
+                    <input name="" id="btborrar" class="btn btn-danger" type="button" value="Editar"/>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+    </div>
+
+</div>
 
 <?php include("../../templates/footer.php");?>
